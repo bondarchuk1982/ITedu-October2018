e929fd0553ae27996441023780c2b903e353367c
bondarchuk1982 <bondarchuk.sa.1982@gmail.com>
2018-10-24 13:01:55 +0300

    Оптимизирована работа main.cpp 1. Теперь проверяется успешность вызова функции создания или записи файла имён. 2. Проверяется успешность чтения файла имён и наличие данных в нём.
    В случае сбоя работы одной из функций по работе с файлом или отсутствия
    данных в файле программа прекращает работу.
    HW_2

--- HW2/Softserve task/SoftServeTask/main.cpp
+++ HW2/Softserve task/SoftServeTask/main.cpp
@@ -6,20 +6,23 @@ using namespace std;
 int main()
 {
     SName sName; // Создаём объект класса
-    sName.creatFile(); // Создаём или записываем в файл рандомные имена(5000)
 
-    std::string str; // Строка для имён из файла
+    if (sName.creatFile()) { // Создаём или записываем в файл рандомные имена(5000)
+        std::string str; // Строка для имён из файла
 
-    sName.rFile(str); // Читаем содержимое файла
-    std::list<std::string> strList; // Контейнер для имён
+        if (sName.rFile(str) && str != "") { // Читаем содержимое файла
+            std::list<std::string> strList; // Контейнер для имён
 
-    sName.parseString(str, strList); // Разбираем содержимое строки
-    sName.arrayInTerminal(strList); // Выводим в терминал имена из контейнера
-    strList.sort(); // Сортируем контейнер встроенной функцией сортировки
-    sName.arrayInTerminal(strList); // Выводим в терминал имена из контейнера
+            sName.parseString(str, strList); // Разбираем содержимое строки
+            sName.arrayInTerminal(strList); // Выводим в терминал имена из контейнера
 
-    std::cout << "**********************************************************\n";
-    printf("%.0f \n", sName.nameToNumber(strList)); // Выщитываем число для всех имён
+            strList.sort(); // Сортируем контейнер встроенной функцией сортировки
+            sName.arrayInTerminal(strList); // Выводим в терминал имена из контейнера
+
+            std::cout << "**********************************************************\n";
+            printf("%.0f \n", sName.nameToNumber(strList)); // Выщитываем число для всех имён
+        }
+    }
 
     return 0;
 }
